a60ffd3cf4675402e0151cd7cc8addb3192ed71d
bradywillmer <bradywillmer@10.232.135.181>
2022-09-08 14:07:05 -0500

    hi

---
+++ src/studio1/LeapYear.java
@@ -0,0 +1,16 @@
+package studio1;
+
+import java.util.Scanner;
+
+public class LeapYear {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		(System.out).println("Is this a leap year?");
+		System.out.print("Enter the year");
+		int year=in.nextInt();
+		boolean isItALeapYear=((year%4==0)&&((year%100!=0)||(year%400==0)));
+		System.out.println(isItALeapYear);
+	}
+
+}
